716ff3a64a6eb5c2a2429b3ebf335e6b3b022684
Daniel <danielwu116@gmail.com>
2021-10-29 15:10:21 -0400

    Add LoginComponent with route

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask 
+from flask.helpers import send_file
 import os
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+
+@app.route('/')
+def index():
+  return send_file('frontend/dist/index.html')
